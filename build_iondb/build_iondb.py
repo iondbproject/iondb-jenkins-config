6d7ee731906d8926b427859fce34119a3e942746
Kris Wallperington <Honibannchi@gmail.com>
2016-05-30 13:55:09 -0700

    Add folder structure. Add desktop build script.

---
+++ build_iondb/build_iondb.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import os
+
+os.system('git clone https://github.com/iondbproject/iondb.git --recursive')
+os.chdir('iondb')
+os.system('git checkout development')
+os.system('git submodule init')
+os.system('git submodule update --remote')
+os.system('mkdir build')
+os.chdir('build')
+os.system('cmake ..')
+os.system('make all')
+os.chdir('bin')
+
+for toexecute in os.listdir('.'):
+        os.system(toexecute)
