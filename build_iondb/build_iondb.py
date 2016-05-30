4b0e8e7216366bec7fa1706fb126d2577094c8a5
Kris Wallperington <Honibannchi@gmail.com>
2016-05-30 15:53:37 -0700

    Temporary change for linear hash fix

--- build_iondb/build_iondb.py
+++ build_iondb/build_iondb.py
@@ -4,7 +4,7 @@ import os
 
 os.system('git clone https://github.com/iondbproject/iondb.git --recursive')
 os.chdir('iondb')
-os.system('git checkout development')
+os.system('git checkout linear-hash-fix')
 os.system('git submodule init')
 os.system('git submodule update --remote')
 os.system('mkdir build')
