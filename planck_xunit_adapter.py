5f10fd003b377c60557f155ee3d6698775982195
Kris Wallperington <Honibannchi@gmail.com>
2016-05-31 11:31:25 -0700

    Add test_iondb script. Move resource files. Update gitignore to exclude pycache.

--- planck_xunit_adapter.py
+++ planck_xunit_adapter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import re
+import sys
 
 def writeXunitTag(tagname, attributes=None):
     if attributes is None:
@@ -10,10 +11,14 @@ def writeXunitTag(tagname, attributes=None):
         print("{key}=\"{val}\" ".format(key=key, val=val), end="")
     print(">")
 
-testcases = []
-summary = {}
-with open("sample_output_bad.txt", "r+") as of:
-    for line in of:
+def adaptPlanckFile(targetfile, destinationfile=None):
+    oldstdout = sys.stdout
+    if destinationfile is not None:
+        sys.stdout = destinationfile
+
+    testcases = []
+    summary = {}
+    for line in targetfile:
         line = line.strip()
 
         matchobj = re.search(r"<test>line:\"(?P<line>.*?)\",file:\"(?P<file>.*?)\",function:\"(?P<function>.*?)\",message:\"(?P<message>.*?)\"<\/test>", line)
@@ -24,46 +29,49 @@ with open("sample_output_bad.txt", "r+") as of:
         if summobj:
             summary = summobj.groupdict()
 
-# for test in testcases:
-#     print(test)
-# print()
-# print(summary)
+    # for test in testcases:
+    #     print(test)
+    # print()
+    # print(summary)
 
-summary["total_failed"] = int(summary["total_tests"]) - int(summary["total_passed"])
+    summary["total_failed"] = int(summary["total_tests"]) - int(summary["total_passed"])
 
-suiteattrs = {
-    "name": "PlanckUnit Output",
-    "reports": 1,
-    "test-cases": len(testcases),
-    "tests": len(testcases),
-    "skip": 0,
-    "errors": summary["total_failed"],
-    "errors-detail": summary["total_failed"],
-    "failures": summary["total_failed"],
-    "failures-detail": summary["total_failed"],
-}
-
-writeXunitTag("test-suite", suiteattrs)
-
-for i,case in enumerate(testcases):
-    caseattrs = {
-        "tests": 1,
-        "errors": 0 if case["line"] == "-1" else 1,
-        "failures": 0 if case["line"] == "-1" else 1,
-        "name": "Test #{}".format(i),
-        "label": "A unit test",
+    suiteattrs = {
+        "name": "PlanckUnit Output",
+        "reports": 1,
+        "test-cases": len(testcases),
+        "tests": len(testcases),
+        "skip": 0,
+        "errors": summary["total_failed"],
+        "errors-detail": summary["total_failed"],
+        "failures": summary["total_failed"],
+        "failures-detail": summary["total_failed"],
     }
 
-    writeXunitTag("test-case", caseattrs)
+    writeXunitTag("test-suite", suiteattrs)
 
-    if case["line"] != "-1": #if fail
-        writeXunitTag("error")
-        print("Failed in function '{func}', at {filen}:{line}: {msg}\n".format(func=case["function"],
-                                                                               filen=case["file"],
-                                                                               line=case["line"],
-                                                                               msg=case["message"]))
-        writeXunitTag("/error")
+    for i,case in enumerate(testcases):
+        caseattrs = {
+            "tests": 1,
+            "errors": 0 if case["line"] == "-1" else 1,
+            "failures": 0 if case["line"] == "-1" else 1,
+            "name": "Test #{}".format(i),
+            "label": "A unit test",
+        }
 
-    writeXunitTag("/test-case")
+        writeXunitTag("test-case", caseattrs)
 
-writeXunitTag("/test-suite")
+        if case["line"] != "-1": #if fail
+            writeXunitTag("error")
+            print("Failed in function '{func}', at {filen}:{line}: {msg}\n".format(func=case["function"],
+                                                                                   filen=case["file"],
+                                                                                   line=case["line"],
+                                                                                   msg=case["message"]))
+            writeXunitTag("/error")
+
+        writeXunitTag("/test-case")
+
+    writeXunitTag("/test-suite")
+
+    # Clean-up changes
+    sys.stdout = oldstdout
---
+++ test_iondb/test_iondb.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import os.path
+
+sys.path.append("../")
+
+import planck_xunit_adapter as pxa
+
+BIN_PATH = "../../build_iondb/iondb/build/bin/"
+
+print("Start IonDB Test Phase.")
+
+os.makedirs("output/", exist_ok=True)
+os.chdir("output/")
+
+for testexec in os.listdir(BIN_PATH):
+    abstest_path = os.path.join(BIN_PATH, testexec)
+    planck_outputfname = "planckunit_{testname}_output.txt".format(testname=testexec)
+    xunit_outputfname = "xunit_{testname}_output.txt".format(testname=testexec)
+    os.system("chmod +x {execfp}".format(execfp=abstest_path))
+    os.system("{testfp} | tee {planckfp}".format(testfp=abstest_path, planckfp=planck_outputfname))
+
+    print("Adapting {pl} -> {xl}...".format(pl=planck_outputfname, xl=xunit_outputfname))
+    with open(planck_outputfname, "r+") as planck_file, open(xunit_outputfname, "w+") as xunit_file:
+        pxa.adaptPlanckFile(planck_file, xunit_file)
+
+print("Test IonDB done.")
