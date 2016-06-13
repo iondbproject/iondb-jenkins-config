0bb904924986212aeb5eb34f5136b8a5f63a3e87
Kris Wallperington <Honibannchi@gmail.com>
2016-06-13 15:51:29 -0700

    Add simple signal sender script.

---
+++ signalsend.py
@@ -0,0 +1,14 @@
+#!python3
+
+import serial
+import sys
+import time
+
+baud = 9600
+port = sys.argv[1]
+msg = sys.argv[2]
+
+ser = serial.Serial(port, baud, timeout=10)
+time.sleep(2)
+ser.write(msg.encode("ascii"))
+ser.close()
