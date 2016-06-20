892427eea20d76f150e6ff98a237b8117ea7a753
Wade Penson <wadepenson@gmail.com>
2016-06-20 14:25:41 -0700

    Revert back to file as stderr

--- pc_post_build.py
+++ pc_post_build.py
@@ -27,16 +27,16 @@ proc = subprocess.Popen(command, **arguments)
 helper_functions.process_output_stream(proc)
 
 # Build Doxygen
-command = ['doxygen', os.path.join(configuration.project_path, 'documentation/doxygen/iondb_template'), '2>',
-		   os.path.join(configuration.pc_output_path, 'doxygen.log')]
-proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
-helper_functions.process_output_stream(proc)
+command = ['doxygen', os.path.join(configuration.project_path, 'documentation/doxygen/iondb_template')]
+with open(os.path.join(configuration.pc_output_path, 'doxygen.log'), 'w') as err:
+	proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=err, universal_newlines=True)
+	helper_functions.process_output_stream(proc)
 
 # Run Cppcheck
 command = ['cppcheck', '--enable=all', '--inconclusive', '--xml', '--xml-version=2',
 		   os.path.join(configuration.project_path, 'src')]
 with open(os.path.join(configuration.pc_output_path, 'cppcheck.xml'), 'w') as err:
-	proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=err, universal_newlines=True)
+	proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=err, universal_newlines=True)
 	helper_functions.process_output_stream(proc)
 
 # Run Dr. Memory
