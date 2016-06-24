4cfd23cca8cd07d6ba86b8a11d2864ce22a3b9bb
Kris Wallperington <Honibannchi@gmail.com>
2016-06-24 16:36:38 -0700

    Add doxygen adapter, rapid flag for pc_post_build

---
+++ helper_files/doxygen_adapter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import re
+import sys
+import functools
+
+graph_re = re.compile(r'(\w+_GRAPH\s*=).*')
+
+def doxy_adapt(filein, fileout=sys.stdout):
+    doxy_print = functools.partial(print, file=fileout)
+
+    for line in filein:
+        line = line.strip()
+        if not line.startswith('#'):
+            graph_obj = graph_re.match(line)
+            if graph_obj:
+                doxy_print(graph_obj.group(1))
+                continue
+
+            doxy_print(line)
+
+if __name__ == '__main__':
+    import fileinput
+    doxy_adapt(fileinput.input())
--- pc_post_build.py
+++ pc_post_build.py
@@ -4,15 +4,22 @@ import subprocess
 import glob
 import sys
 import logging
+import argparse
 
 import configuration
 
 sys.path.append('helper_files/')
 import helper_functions
+import doxygen_adapter
 
 logger = logging.getLogger(__name__)
 logger.addHandler(configuration.pc_logger)
 logger.addHandler(configuration.console_logger)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--rapid", metavar="", action='store_true',
+                    help="Whether or not to do a rapid build. (Disables many slow profilers)")
+p_args = parser.parse_args()
 
 arguments = {'stdout': subprocess.PIPE, 'stderr': subprocess.STDOUT, 'universal_newlines': True}
 
@@ -27,7 +34,16 @@ proc = subprocess.Popen(command, **arguments)
 helper_functions.process_output_stream(proc, logging.INFO)
 
 # Build Doxygen
+iondb_doxy_path = os.path.join(configuration.project_path,'documentation/doxygen/iondb_template')
 command = ['doxygen', 'iondb_template']
+if p_args.rapid:
+	# If we're a rapid build, we skip all graph generation
+	rapid_doxy_path = os.path.join(configuration.project_path,'documentation/doxygen/rapid_template')
+	with open(iondb_doxy_path, 'r') as doxy_temp, open(rapid_doxy_path, 'w') as rapid_temp:
+		doxygen_adapter.doxy_adapt(doxy_temp, rapid_temp)
+
+	command = ['doxygen', 'rapid_template']
+
 with open(os.path.join(configuration.pc_output_path, 'doxygen.log'), 'w') as err:
 	proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=err, universal_newlines=True,
 							cwd=os.path.join(configuration.project_path, 'documentation/doxygen'))
