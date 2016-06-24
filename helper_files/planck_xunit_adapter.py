83ee1c6e33d3258f93986a50736eed2760de4fe9
Kris Wallperington <Honibannchi@gmail.com>
2016-06-24 10:30:14 -0700

    Fix wrong loc of PXA msg output. Clean up PXA

--- helper_files/planck_xunit_adapter.py
+++ helper_files/planck_xunit_adapter.py
@@ -3,6 +3,8 @@ import sys
 import re
 import string
 import logging
+import functools
+from contextlib import contextmanager
 
 sys.path.append('../')
 import configuration
@@ -15,22 +17,29 @@ logger.addHandler(configuration.console_logger)
 
 # Outputs PlanckUnit test results in JUnit XML spec.
 class PlanckAdapter:
-	def __init__(self, suite_name, target_file, destination_file=None):
+	def __init__(self, suite_name, target_file, destination_file=sys.stdout):
 		self.suite_name = suite_name
 		self.target_file = target_file
-		self.destination_file = destination_file if destination_file is not None else sys.stdout
+		self.destination_file = destination_file
+		# This is a wrapper around the print function that redirects output to dest. file.
+		self.pxa_print = functools.partial(print, file=self.destination_file)
 
 	def write_xml_header(self):
-		print('<?xml version="1.0" encoding="UTF-8"?>', file=self.destination_file)
+		self.pxa_print('<?xml version="1.0" encoding="UTF-8"?>')
 
+	@contextmanager
 	def write_xunit_tag(self, tag_name, attributes=None):
 		if attributes is None:
 			attributes = {}
 
-		print('<{tag} '.format(tag=tag_name), end='', file=self.destination_file)
+		self.pxa_print('<{tag} '.format(tag=tag_name), end='')
 		for key,val in attributes.items():
-			print('{key}=\"{val}\" '.format(key=key, val=val), end='', file=self.destination_file)
-		print('>', file=self.destination_file)
+			self.pxa_print('{key}=\"{val}\" '.format(key=key, val=val), end='')
+		self.pxa_print('>')
+
+		yield
+
+		self.pxa_print('</{tag}>'.format(tag=tag_name))
 
 	def adapt_planck_file(self):
 		test_case_re = re.compile(r'<test>line:\"(?P<line>.*?)\",file:\"(?P<file>.*?)\",function:\"(?P<function>.*?)\",message:\"(?P<message>.*?)\"<\/test>')
@@ -68,41 +77,42 @@ class PlanckAdapter:
 		}
 
 		self.write_xml_header()
-		self.write_xunit_tag('testrun', runattrs)
-		self.write_xunit_tag('testsuite', {'name': self.suite_name, 'time': '0.0'})
+		with self.write_xunit_tag('testrun', runattrs):
+			with self.write_xunit_tag('testsuite', {'name': self.suite_name, 'time': '0.0'}):
+				# If PlanckSerial reported an error, then we want to write a special 'testcase' for this error.
+				if error_msg:
+					error_attrs = {
+						'name': '_planck_error',
+						'classname': self.suite_name,
+						'time': '0.0',
+					}
 
-		# If PlanckSerial reported an error, then we want to write a special 'testcase' for this error.
-		if error_msg:
-			error_attrs = {
-				'name': '_planck_error',
-				'classname': self.suite_name,
-				'time': '0.0',
-			}
+					with self.write_xunit_tag('testcase', error_attrs):
+						with self.write_xunit_tag('failure'):
+							self.pxa_print(error_msg)
 
-			self.write_xunit_tag('testcase', error_attrs)
-			self.write_xunit_tag('failure')
-			print(error_msg, file=self.destination_file)
-			self.write_xunit_tag('/failure')
-			self.write_xunit_tag('/testcase')
+				for case in test_cases:
+					case_attrs = {
+						'name': '{funcname}'.format(funcname=case['function']),
+						'classname': self.suite_name,
+						'time': '0.0',
+					}
 
-		for case in test_cases:
-			case_attrs = {
-				'name': '{funcname}'.format(funcname=case['function']),
-				'classname': self.suite_name,
-				'time': '0.0',
-			}
+					with self.write_xunit_tag('testcase', case_attrs):
+						if case['line'] != '-1': #if fail
+							with self.write_xunit_tag('failure'):
+								self.pxa_print(
+									'Failed in function "{func}", at {filen}:{line}: {msg}'.format(
+										func=case['function'],
+										filen=case['file'],
+										line=case['line'],
+										msg=case['message']
+									)
+								)
 
-			self.write_xunit_tag('testcase', case_attrs)
 
-			if case['line'] != '-1': #if fail
-				self.write_xunit_tag('failure')
-				logger.error('Failed in function "{func}", at {filen}:{line}: {msg}\n'.format(func=case['function'],
-																					   filen=case['file'],
-																					   line=case['line'],
-																					   msg=case['message']))
-				self.write_xunit_tag('/failure')
+if __name__ == '__main__':
+	import fileinput
 
-			self.write_xunit_tag('/testcase')
-
-		self.write_xunit_tag('/testsuite')
-		self.write_xunit_tag('/testrun')
+	with fileinput.input() as fin:
+		PlanckAdapter("Terminal test", fin).adapt_planck_file()
